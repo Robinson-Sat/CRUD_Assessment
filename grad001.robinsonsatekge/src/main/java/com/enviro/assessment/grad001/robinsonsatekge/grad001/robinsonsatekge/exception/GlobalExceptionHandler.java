6e531c6485029ad675c96997cd669d64d793a74a
Robonsin-Sat <Robinson.Satekge@gmail.com>
2024-03-30 20:35:21 +0200

    Pushing to github

---
+++ grad001.robinsonsatekge/src/main/java/com/enviro/assessment/grad001/robinsonsatekge/grad001/robinsonsatekge/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.enviro.assessment.grad001.robinsonsatekge.grad001.robinsonsatekge.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RecordNotFoundException.class)
+    public ResponseEntity<String> handleRecordNotFoundException(RecordNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
