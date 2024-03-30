5045eecc1ab41c36b68fb7e464a9415dea725210
Robinson-Sat <156796180+Robinson-Sat@users.noreply.github.com>
2024-03-30 21:10:03 +0200

    Add files via upload

---
+++ grad001.robinsonsatekge/src/main/java/com/enviro/assessment/grad001/robinsonsatekge/grad001/robinsonsatekge/exception/RecordNotFoundException.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.robinsonsatekge.grad001.robinsonsatekge.exception;
+
+public class RecordNotFoundException extends RuntimeException {
+    public RecordNotFoundException(String message) {
+        super(message);
+    }
+}
