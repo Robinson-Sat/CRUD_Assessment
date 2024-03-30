15ddea74ca25304195a0d99c070659dccfd76942
Robinson-Sat <156796180+Robinson-Sat@users.noreply.github.com>
2024-03-30 20:57:03 +0200

    Delete grad001.robinsonsatekge directory

--- grad001.robinsonsatekge/src/main/java/com/enviro/assessment/grad001/robinsonsatekge/grad001/robinsonsatekge/exception/RecordNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.enviro.assessment.grad001.robinsonsatekge.grad001.robinsonsatekge.exception;
-
-public class RecordNotFoundException extends RuntimeException {
-    public RecordNotFoundException(String message) {
-        super(message);
-    }
-}
--- grad001.robinsonsatekge/src/main/java/com/enviro/assessment/grad001/robinsonsatekge/grad001/robinsonsatekge/repository/RecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.enviro.assessment.grad001.robinsonsatekge.grad001.robinsonsatekge.repository;
-
-
-import com.enviro.assessment.grad001.robinsonsatekge.grad001.robinsonsatekge.model.MyRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface RecordRepository extends JpaRepository<MyRecord, Long> {
-}
